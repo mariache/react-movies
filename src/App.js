17ea68a7e8e5b24c2d3051140333f794d8f8fb22
Salam Omarbagaev <omarbagaev@gmail.com>
2020-07-03 00:03:17 +1200

    updated app js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import React from "react";
+import Filters from "./Filters/Filters";
+import MoviesList from "./Movies/MoviesList";
 
-import "./App.css";
-
-function App() {
-  return <div className="App">React Movies App</div>;
+export default class App extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="row mt-4">
+          <div className="col-4">
+            <div className="card" style={{ width: "100%" }}>
+              <div className="card-body">
+                <h3>Filters:</h3>
+                <Filters />
+              </div>
+            </div>
+          </div>
+          <div className="col-8">
+            <MoviesList />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
-
-export default App;

c5459659f280deafa51b67693a9d4ef5b8d8acce
Salam Omarbagaev <omarbagaev@gmail.com>
2020-07-04 23:55:51 +1200

    Movies functional component instead of class; used useState.useEfeect;

--- src/Movies/MoviesList.js
+++ src/Movies/MoviesList.js
@@ -1,95 +1,73 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import MovieItem from "./MovieItem";
 import { API_URL, API_KEY_3 } from "../api/api";
 import MoviesTabs from "./MoviesTabs";
 
-export default class MoviesList extends Component {
-  constructor() {
-    super();
+export const MoviesList = () => {
+  const [movies, setMovies] = useState([]);
+  const [watchLater, setWatchLater] = useState([]);
+  const [sortBy, setSortBy] = useState("popularity.desc");
 
-    this.state = {
-      movies: [],
-      watchLater: [],
-      sortBy: "popularity.desc",
-    };
-  }
-
-  getMovies = () => {
-    const link = `${API_URL}/discover/movie?api_key=${API_KEY_3}&sort_by=${this.state.sortBy}&language=en-EN`;
+  const getMovies = () => {
+    const link = `${API_URL}/discover/movie?api_key=${API_KEY_3}&sort_by=${sortBy}&language=en-EN`;
     fetch(link)
       .then((response) => {
         return response.json();
       })
       .then((data) => {
-        this.setState({
-          movies: data.results,
-        });
+        setMovies(data.results);
       });
   };
 
-  componentDidMount() {
-    this.getMovies();
-  }
+  useEffect(() => {
+    getMovies();
+  }, []);
 
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState.sortBy !== this.state.sortBy) {
-      this.getMovies();
-    }
-  }
-
-  onRemove = (id) => {
+  const onRemove = (id) => {
     const updatedMovies = this.state.movies.slice().filter((x) => x.id !== id);
-    this.setState({ movies: updatedMovies });
+    setMovies(updatedMovies);
   };
 
-  onWatchLater = (movie) => {
-    const watchedLater = [...this.state.watchLater, movie];
-    this.setState({
-      watchLater: watchedLater,
-    });
+  const onWatchLater = (movie) => {
+    const watchedLater = [...watchLater, movie];
+    setWatchLater(watchedLater);
   };
 
-  onRemoveWatchLater = (id) => {
-    const watchedLater = this.state.watchLater
-      .slice()
-      .filter((x) => x.id !== id);
-    this.setState({ watchLater: watchedLater });
+  const onRemoveWatchLater = (id) => {
+    const watchedLater = watchLater.slice().filter((x) => x.id !== id);
+    setWatchLater(watchedLater);
   };
 
-  updateSortBy = (value) => {
-    this.setState({ sortBy: value });
+  const updateSortBy = (value) => {
+    setSortBy(value);
   };
 
-  render() {
-    const { movies, watchLater } = this.state;
-    return (
-      <div className="container">
-        <div className="row mb-4">
-          <div className="col-12">
-            <MoviesTabs
-              sortBy={this.state.sortBy}
-              updateSortBy={this.updateSortBy}
-            />
-          </div>
-        </div>
-        <div className="row">
-          {movies.map((movie) => {
-            return (
-              <div key={movie.id} className="col-6 mb-4">
-                <MovieItem
-                  item={movie}
-                  onRemove={this.onRemove}
-                  onWatchLater={this.onWatchLater}
-                  onRemoveWatchLater={this.onRemoveWatchLater}
-                />
-              </div>
-            );
-          })}
-          <div className="col-3">
-            <p>Will watch: {watchLater.length}</p>
-          </div>
+  return (
+    <div className="container">
+      <div className="row mb-4">
+        <div className="col-12">
+          <MoviesTabs sortBy={sortBy} updateSortBy={updateSortBy} />
         </div>
       </div>
-    );
-  }
-}
+      <div className="row">
+        {movies.map((movie) => {
+          return (
+            <div key={movie.id} className="col-6 mb-4">
+              <MovieItem
+                item={movie}
+                onRemove={onRemove}
+                onWatchLater={onWatchLater}
+                onRemoveWatchLater={onRemoveWatchLater}
+              />
+            </div>
+          );
+        })}
+        <div className="col-3">
+          <p>Will watch: {watchLater.length}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MoviesList;

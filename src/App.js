a14eb2df2dd67014f1af1631779b3143c98e3ff0
Salam Omarbagaev <omarbagaev@gmail.com>
2020-07-05 23:25:39 +1200

    markup change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export default class App extends React.Component {
     return (
       <div className="container">
         <div className="row mt-4">
-          <div className="col-4">
+          <div className="col-3">
             <div className="card" style={{ width: "100%" }}>
               <div className="card-body">
                 <h3>Filters:</h3>
@@ -15,7 +15,7 @@ export default class App extends React.Component {
               </div>
             </div>
           </div>
-          <div className="col-8">
+          <div className="col-9">
             <MoviesList />
           </div>
         </div>
--- src/Movies/MovieItem.js
+++ src/Movies/MovieItem.js
@@ -25,38 +25,42 @@ export const MovieItem = ({
 
   return (
     <div className="card" style={{ width: "100%" }}>
-      <img
-        className="card-img-top card-img--height"
-        src={
-          item.poster_path !== null
-            ? `https://image.tmdb.org/t/p/w500${
-                item.backdrop_path || item.poster_path
-              }`
-            : defaultImage
-        }
-        alt="movie"
-      />
-      <div className="card-body card">
-        <h6 className="card-title">{item.title}</h6>
-        <div className="d-flex justify-content-between align-items-center">
-          <p className="mb-0">Rating: {item.vote_average}</p>
-          {watchLater ? (
-            <i
-              className="fa fa-bookmark"
-              style={{ cursor: "pointer", color: "#EAA221" }}
-              onClick={onHandleRemoveWatchLater(item.id)}
-            ></i>
-          ) : (
-            <i
-              className="fa fa-bookmark-o"
-              style={{ cursor: "pointer" }}
-              onClick={onHandleWatchLater(item)}
-            ></i>
-          )}
+      <div className="card-body card-movie">
+        <div className="card-movie__img">
+          <img
+            className="card-img-top card-img--height"
+            src={
+              item.poster_path !== null
+                ? `https://image.tmdb.org/t/p/w500${
+                    item.backdrop_path || item.poster_path
+                  }`
+                : defaultImage
+            }
+            alt="movie"
+          />
         </div>
-        <button className="btn btn-danger" onClick={onHandleRemove(item.id)}>
-          Delete
-        </button>
+        <div className="card-movie__description">
+          <h6 className="card-title">{item.title}</h6>
+          <div className="d-flex justify-content-between align-items-center">
+            <p className="mb-0">Rating: {item.vote_average}</p>
+            {watchLater ? (
+              <i
+                className="fa fa-bookmark"
+                style={{ cursor: "pointer", color: "#EAA221" }}
+                onClick={onHandleRemoveWatchLater(item.id)}
+              ></i>
+            ) : (
+              <i
+                className="fa fa-bookmark-o"
+                style={{ cursor: "pointer" }}
+                onClick={onHandleWatchLater(item)}
+              ></i>
+            )}
+          </div>
+          <button className="btn btn-danger" onClick={onHandleRemove(item.id)}>
+            Delete
+          </button>
+        </div>
       </div>
     </div>
   );

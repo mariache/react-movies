2c1cb511cf310a1f9cc227a3217ee58f11ff9995
Salam Omarbagaev <omarbagaev@gmail.com>
2020-07-06 22:41:27 +1200

    - App and Filters are functional components

--- src/App.js
+++ src/App.js
@@ -2,24 +2,23 @@ import React from "react";
 import Filters from "./Filters/Filters";
 import MoviesList from "./Movies/MoviesList";
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <div className="container">
-        <div className="row mt-4">
-          <div className="col-3">
-            <div className="card" style={{ width: "100%" }}>
-              <div className="card-body">
-                <h3>Filters:</h3>
-                <Filters />
-              </div>
+export const App = () => {
+  return (
+    <div className="container">
+      <div className="row mt-4">
+        <div className="col-3">
+          <div className="card" style={{ width: "100%" }}>
+            <div className="card-body">
+              <h3>Filters:</h3>
+              <Filters />
             </div>
           </div>
-          <div className="col-9">
-            <MoviesList />
-          </div>
+        </div>
+        <div className="col-9">
+          <MoviesList />
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+export default App;
--- src/Filters/Filters.js
+++ src/Filters/Filters.js
@@ -1,19 +1,19 @@
 import React from "react";
 
-export default class Filters extends React.Component {
-  render() {
-    return (
-      <form className="mb-3">
-        <div className="form-group">
-          <label htmlFor="sort_by">Order by:</label>
-          <select className="form-control" id="sort_by">
-            <option value="popularity.desc">Popular desc</option>
-            <option value="popularity.asc">Popular asc</option>
-            <option value="vote_average.desc">Rate desc</option>
-            <option value="vote_average.asc">Rate asc</option>
-          </select>
-        </div>
-      </form>
-    );
-  }
-}
+export const Filters = (props) => {
+  return (
+    <form className="mb-3">
+      <div className="form-group">
+        <label htmlFor="sort_by">Order by:</label>
+        <select className="form-control" id="sort_by">
+          <option value="popularity.desc">Popularity desc</option>
+          <option value="popularity.asc">Popularity asc</option>
+          <option value="vote_average.desc">Rating desc</option>
+          <option value="vote_average.asc">Rating asc</option>
+        </select>
+      </div>
+    </form>
+  );
+};
+
+export default Filters;

f4d8776f68e1aada066ad9349e6d4004da06b316
Salam Omarbagaev <omarbagaev@gmail.com>
2020-07-06 22:42:10 +1200

    moved state to parent

--- src/App.js
+++ src/App.js
@@ -1,8 +1,70 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Filters from "./Filters/Filters";
 import MoviesList from "./Movies/MoviesList";
+import { API_URL, API_KEY_3 } from "./api/api";
+
+const useMoviesWatchLater = () => {
+  const [watchLater, setWatchLater] = useState([]);
+  const onWatchLater = (movie) => {
+    const watchedLater = [...watchLater, movie];
+    setWatchLater(watchedLater);
+  };
+
+  const onRemoveWatchLater = (id) => {
+    const watchedLater = watchLater.slice().filter((x) => x.id !== id);
+    setWatchLater(watchedLater);
+  };
+  return {
+    watchLater,
+    onWatchLater,
+    onRemoveWatchLater,
+  };
+};
+
+const useMovies = () => {
+  const [movies, setMovies] = useState([]);
+  const getMovies = ({ sortBy }) => {
+    const link = `${API_URL}/discover/movie?api_key=${API_KEY_3}&sort_by=${sortBy}&language=en-EN`;
+    fetch(link)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setMovies(data.results);
+      });
+  };
+
+  const onRemove = (id) => {
+    const updatedMovies = movies.slice().filter((x) => x.id !== id);
+    setMovies(updatedMovies);
+  };
+
+  return {
+    movies,
+    getMovies,
+    onRemove,
+  };
+};
 
 export const App = () => {
+  const [sortBy, setSortBy] = useState("popularity.desc");
+
+  const {
+    watchLater,
+    onWatchLater,
+    onRemoveWatchLater,
+  } = useMoviesWatchLater();
+
+  const { movies, getMovies, onRemove } = useMovies();
+
+  useEffect(() => {
+    getMovies({ sortBy });
+    // eslint-disable-next-line
+  }, [sortBy]);
+
+  const updateSortBy = (value) => {
+    setSortBy(value);
+  };
   return (
     <div className="container">
       <div className="row mt-4">
@@ -15,10 +77,19 @@ export const App = () => {
           </div>
         </div>
         <div className="col-9">
-          <MoviesList />
+          <MoviesList
+            movies={movies}
+            sortBy={sortBy}
+            updateSortBy={updateSortBy}
+            onRemove={onRemove}
+            onRemoveWatchLater={onRemoveWatchLater}
+            onWatchLater={onWatchLater}
+            watchLater={watchLater}
+          />
         </div>
       </div>
     </div>
   );
 };
+
 export default App;
--- src/Movies/MoviesList.js
+++ src/Movies/MoviesList.js
@@ -1,72 +1,18 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import MovieItem from "./MovieItem";
-import { API_URL, API_KEY_3 } from "../api/api";
+
 import MoviesTabs from "./MoviesTabs";
 import { TransitionGroup, CSSTransition } from "react-transition-group";
 
-const useMoviesWatchLater = () => {
-  const [watchLater, setWatchLater] = useState([]);
-  const onWatchLater = (movie) => {
-    const watchedLater = [...watchLater, movie];
-    setWatchLater(watchedLater);
-  };
-
-  const onRemoveWatchLater = (id) => {
-    const watchedLater = watchLater.slice().filter((x) => x.id !== id);
-    setWatchLater(watchedLater);
-  };
-  return {
-    watchLater,
-    onWatchLater,
-    onRemoveWatchLater,
-  };
-};
-
-const useMovies = () => {
-  const [movies, setMovies] = useState([]);
-  const getMovies = ({ sortBy }) => {
-    const link = `${API_URL}/discover/movie?api_key=${API_KEY_3}&sort_by=${sortBy}&language=en-EN`;
-    fetch(link)
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        setMovies(data.results);
-      });
-  };
-
-  const onRemove = (id) => {
-    const updatedMovies = movies.slice().filter((x) => x.id !== id);
-    setMovies(updatedMovies);
-  };
-
-  return {
-    movies,
-    getMovies,
-    onRemove,
-  };
-};
-
-export const MoviesList = () => {
-  const [sortBy, setSortBy] = useState("popularity.desc");
-
-  const {
-    watchLater,
-    onWatchLater,
-    onRemoveWatchLater,
-  } = useMoviesWatchLater();
-
-  const { movies, getMovies, onRemove } = useMovies();
-
-  useEffect(() => {
-    getMovies({ sortBy });
-    // eslint-disable-next-line
-  }, [sortBy]);
-
-  const updateSortBy = (value) => {
-    setSortBy(value);
-  };
-
+export const MoviesList = ({
+  movies,
+  sortBy,
+  updateSortBy,
+  onRemove,
+  onRemoveWatchLater,
+  onWatchLater,
+  watchLater,
+}) => {
   return (
     <div className="container">
       <div className="row mb-4">

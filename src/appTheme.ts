9c4bf1462b983e429648894b117682371e9617ed
Salam Omarbagaev <omarbagaev@gmail.com>
2020-07-08 00:04:03 +1200

    - appTheme file

---
+++ src/appTheme.ts
@@ -0,0 +1,17 @@
+export const appTheme = {
+  defaultFontStyle: { fontFamily: "'Open Sans', sans-serif;" },
+  palette: {
+    themeDark: "#212529",
+    dark: "#081c24",
+    green: "#21D07A",
+    yellow: "#D0D331",
+    red: "#E94F37",
+    white: "#FFFFFF",
+    transparent: "transparent",
+  },
+  semanticColors: {
+    error: "#EF3E36",
+    disabledBackground: "#F4F4F4",
+    bodyBackground: "#FFFFFF",
+  },
+};
